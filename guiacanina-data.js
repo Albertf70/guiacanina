8881b67c02193788481572c9856906e16cc10d21
Albertf70 <costingpro7@gmail.com>
2026-02-11 18:28:06 -0600

    Update guiacanina-data.js

--- guiacanina-data.js
+++ guiacanina-data.js
@@ -11,7 +11,7 @@ const BREEDS_DATABASE = [
     nombre: "Chihuahua",
     nombresAlternativos: ["Chi", "Chihuahueño"],
     categoria: "mexicanas",
-    imagen: "chihuahua.jpg",
+    imagen: "🐕",
     tamaño: "Pequeñas",
     
     origen: {
